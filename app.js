c7c3dc9f9549dd7260aa1f9d78bfeafc3eeef822
Vibhu Dixit <vibhudixit.2215001940.com>
2024-10-18 22:10:33 +0530

    till CRUD on products

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//-----------------------------REQUIRE CONFIG-----------------------------
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -7,33 +6,26 @@ const seedDb = require('./seed')
 const productRoutes = require('./routes/product');
 const userRoutes = require('./routes/user');
 const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 
-//-----------------------------FOLDER CONFIG----------------------
+app.use(methodOverride('_method'));
 app.engine('ejs',ejsMate)
 app.set('view engine','ejs');//Iska simple yahi kaam hai ki ejs file ko read kare
 app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
-
-//-----------------------------DB SEEDING-----------------------------
-
+app.use(express.urlencoded({extended:true}))
 // seedDb(); // Seed the database with sample data for testing jsut use one time only
-
-//-----------------------------DB CONFIG-----------------------------
 mongoose.connect('mongodb://127.0.0.1:27017/shopping')
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log('Db Error: ' + err));
-
-//-----------------------------ROUTES CONFIG-----------------------------
 app.get('/', function (req, res) {
     res.send('Raand!!')
 })
 app.use(productRoutes)
 app.use(userRoutes)
-
 app.get('*', function (req, res) {
     res.send('Randibaaz kaha dekh raha hai!!');
 })
-//-----------------------------SERVER CONFIG-----------------------------
 app.listen(8000,()=>{
     console.log('Server is running on port 8000');
 })
--- routes/product.js
+++ routes/product.js
@@ -4,10 +4,45 @@ const express = require('express');
 const router = express.Router();//Mini instance of app .... as app is the instance of application thus we can't export it use here we have to use its mini instance!!
 const Product = require('../models/Product');//For accessing data from the db
 
+//To show all the products
 router.get('/products', async function(req, res) {
     let products = await Product.find()
     res.render('products/index', {products});
 })
+//To show the form for new products
+router.get('/products/new', function(req, res) {
+    res.render('products/new');
+})
 
+//To actually add a new product
+router.post('/products', async function(req, res) {
+    let {name,img,price,desc} = req.body;
+    await Product.create({name,img,price,desc});
+    res.redirect('/products');
+})
+//To show a perticular product
+router.get('/products/:id', async function(req, res) {
+    let {id} = req.params;
+    let foundProduct = await Product.findById(id);
+    res.render('products/show',{foundProduct});
+})
+//Show edit form for edit the product details
+router.get('/products/:id/edit', async function(req, res) {
+    let {id} = req.params;
+    let foundProduct = await Product.findById(id);
+    res.render('products/edit',{foundProduct});
+})
+//To actually edit the product details
+router.patch('/products/:id', async function(req, res) {
+    let {id} = req.params;
+    let updatedProduct = await Product.findByIdAndUpdate(id, req.body, {new: true});
+    res.redirect(`/products`);
+})
+//To delete a product
 
+router.delete('/products/:id', async function(req, res) {
+    let {id} = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
 module.exports = router;
